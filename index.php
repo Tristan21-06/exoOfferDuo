fa0b19c3a5a42f850bc6726320d4c2f1048f7986
admin <admin@VIL-S01-iMac-ITA-16.local>
2023-10-04 17:02:59 +0200

    index & css

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="/assets/css/style.min.css" rel="stylesheet">
+        <title>Accueil</title>
+    </head>
+    <body>
+        <nav id="nav">
+            <div>
+                <a href="#" class="brand-logo"><img src="/assets/img/logo_2x_feoygs.webp" alt="Logo ITAkademy"></a>
+                <ul>
+                    <li>
+                        <a href="/pages/cart.html">
+                            <span class="material-icons">
+                                shopping_cart
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="presentation">
+            
+            <div class="content">
+                <div id="text">
+                    <h2>
+                        Nous allons aujourd'hui mettre en valeur un produit dérivé de la série appelée <em>"Cars"</em>
+                    </h2>
+                    <div>
+                        <h3>
+                            Tout d'abord, qu'est-ce que c'est <em>"Cars"</em>?
+                        </h3>
+                        <p>
+                            C'est un monde peuplé entièrement de véhicules vivants, Flash McQueen, une voiture de course rouge est un jeune champion avide de succès et promis à une très belle carrière. Un jour, il se retrouve abandonné par accident en plein désert et en essayant de retrouver son chemin, arrive dans la petite ville de Radiator Springs, située sur la mythique Route 66. Loin de ses fans, il va s’y faire de nouveaux amis et découvrir qu'il y a des choses bien plus importantes dans la vie que de franchir en premier la ligne d’arrivée.
+                        </p>
+                    </div>
+                </div>
+                <div id="img">
+                    <img src="/assets/img/flash_mcqueen_voiture.webp" alt="Flash McQueen">
+                    <figcaption>Flash McQueen</figcaption>
+                </div>
+            </div>
+            
+        </section>
+
+        <section id="product">
+            
+        </section>
+
+        <script src="/assets/js/cart.min.js"></script>
+    </body>
+</html>
---
+++ pages/payment.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="/assets/css/style.css" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+        <title>Paiement</title>
+    </head>
+    <body>
+        <nav id="nav">
+            <div>
+              <a href="/" class="brand-logo"><img src="/assets/img/logo_2x_feoygs.webp" alt="Logo ITAkademy"></a>
+              <ul id="nav-mobile">
+                <li>
+                    <a href="/pages/cart.html">
+                        <span class="material-icons">
+                            shopping_cart
+                        </span>
+                    </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    </body>
+</html>
